b57335a803942cee2aa250517e74b470f1d52b1f
Mohsin <mdmohsinhaider6@gmail.com>
2023-10-14 13:48:25 +0530

    upload_1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <h1>hello</h1>
+      <header className="Contener_Header">
+        <h1>hello heder</h1>
+      </header>
+      <body>
+        <nav className="Conterner_Nav">this is nav bar</nav>
+        <main className="container_Body">
+          <div className="Left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+            a fugit ab labore animi quo eum explicabo quidem sed dicta. Veniam
+            repellat dignissimos sint porro a tempore obcaecati debitis
+            molestias.
+          </div>
+          <div className="Right">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            explicabo porro totam. Similique quod, a quis, laborum provident
+            harum eaque expedita repellat deleniti ut culpa nostrum? Vitae totam
+            similique perspiciatis.
+          </div>
+        </main>
+        <footer>this is footer</footer>
+      </body>
     </>
-  )
+  );
 }
