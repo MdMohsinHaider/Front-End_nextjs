3de7f9f8ecdea4c17d05b1b4ff90d6c1e3a8accc
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-13 21:58:29 +0530

    backend

---
+++ app/api/users/route.js
@@ -0,0 +1,14 @@
+
+export function GET(){
+    const users=[{
+        name:"devil",
+        phone:12345678
+    }
+]
+};
+
+export function POST(){};
+
+export function DELETE(){};
+
+export function PUT(){};
