63f470c4f1545e6d052c408d9d215710cf87d581
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-13 22:14:17 +0530

    backend

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,5 +1,6 @@
+import { NextResponse } from "next/server"
 
-export function GET() {
+export function GET(request) {
     const users = [
         {
         name: "devil",
@@ -14,10 +15,17 @@ export function GET() {
         phone: 12345678
         }
     ]
+    return NextResponse.json(users);
 };
 
 export function POST() { };
 
-export function DELETE() { };
+export function DELETE(request) {
+    console.log("detete method");
+    return NextResponse.json({
+        massage:"deleted method delete req",
+        status:"true"
+    })
+ };
 
 export function PUT() { };
